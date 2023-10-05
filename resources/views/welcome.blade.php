0787680cfef4e1c293388c5091c24398548ad895
markosantonio19 <1433811@senati.pe>
2023-10-05 11:58:49 -0500

    creacion de pagina web

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    
+    <title>TURISMO WORLD</title>
+</head>
+<body>
+
+
+
+
+
+    <header>
+        <h1>Nombre de tu Destino Turístico</h1>
+        <nav>
+            <ul>
+                <li><a href="#acerca">Acerca del Destino</a></li>
+                <li><a href="#atracciones">Atracciones Turísticas</a></li>
+                <li><a href="#alojamiento">Alojamiento</a></li>
+                <li><a href="#gastronomia">Gastronomía</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <section id="acerca">
+        <h2>Acerca del Destino</h2>
+        <p>Información general sobre el destino turístico.</p>
+    </section>
+    
+    <section id="atracciones">
+        <h2>Atracciones Turísticas</h2>
+        <p>Descripción de las principales atracciones y actividades.</p>
+    </section>
+    
+    <section id="alojamiento">
+        <h2>Alojamiento</h2>
+        <p>Opciones de alojamiento disponibles.</p>
+    </section>
+    
+    <section id="gastronomia">
+        <h2>Gastronomía</h2>
+        <p>Restaurantes locales y platos típicos.</p>
+    </section>
+    
+    <footer>
+        <p>&copy; 2023 Nombre de tu Destino Turístico</p>
+    </footer>
+</body>
+</html>
